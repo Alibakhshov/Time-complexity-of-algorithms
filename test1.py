3507beebf556f48eecf53f0489b3135215250740
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2023-03-16 12:59:22 +0600

    Updates

--- test1.py
+++ test1.py
@@ -1,14 +1,41 @@
-from flask import Flask, render_template, request
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 
-app = Flask(__name__)
+class FirstWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-# Load the dataset
-#df = pd.read_csv('dataset.csv')
+    def initUI(self):
+        self.button = QPushButton('Open Second Window', self)
+        self.button.clicked.connect(self.open_second_window)
 
-# Define the routes for the different tasks
-@app.route('/')
-def home():
-    return render_template('home.html')
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.button)
+        self.setLayout(vbox)
+
+        self.setGeometry(100, 100, 300, 300)
+        self.setWindowTitle('First Window')
+        self.show()
+
+    def open_second_window(self):
+        self.second_window = SecondWindow()
+        self.second_window.show()
+        self.close()
+
+class SecondWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(100, 100, 300, 300)
+        self.setWindowTitle('Second Window')
+
+def main():
+    app = QApplication(sys.argv)
+    window = FirstWindow()
+    sys.exit(app.exec())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
